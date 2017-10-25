85560868c679e6843bc2526e98881ea4270147a0
Villonder <32059608+Villonder@users.noreply.github.com>
2017-10-25 10:29:33 +0200

    Merge branch 'Master' into Eduardo
    # Conflicts:
    #	.idea/vcs.xml

--- app/src/main/java/com/example/rabanales21/rabanales21/MainActivity.java
+++ app/src/main/java/com/example/rabanales21/rabanales21/MainActivity.java
@@ -9,7 +9,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        System.out.println("ok");
+        System.out.println("ok2");
     }
     
     String cadena = "Hola Mundo mhlhoi";
