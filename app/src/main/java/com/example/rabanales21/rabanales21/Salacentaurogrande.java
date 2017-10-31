57cdb5c42910c41f0477fdd935db06e1edb23d08
dgjaenes <32044297+dgjaenes@users.noreply.github.com>
2017-10-31 13:40:00 +0100

    david 31 de octubre

--- app/src/main/java/com/example/rabanales21/rabanales21/Salacentaurogrande.java
+++ app/src/main/java/com/example/rabanales21/rabanales21/Salacentaurogrande.java
@@ -19,16 +19,6 @@ public class Salacentaurogrande extends Fragment {
 
         View v = inflater.inflate(R.layout.fragment_salacentaurogrande, container, false);
 
-        //Nuevos parametros para el view del fragmento
-        RelativeLayout.LayoutParams params =    new RelativeLayout.LayoutParams(
-                RelativeLayout.LayoutParams.MATCH_PARENT,
-                RelativeLayout.LayoutParams.MATCH_PARENT);
-        //Nueva Regla: EL fragmento estara debajo del boton add_fragment
-        params.addRule(RelativeLayout.BELOW, R.id.barratop);
-        //Margenes
-        params.setMargins(0,150,0,0);
-        //Setear los parametros al view
-        v.setLayoutParams(params);
         return v;
     }
 }
