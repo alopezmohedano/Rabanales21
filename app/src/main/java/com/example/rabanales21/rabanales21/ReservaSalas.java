3c602354d410da08fd0490704c3aa0827a578c49
alopezmohedano <32020438+alopezmohedano@users.noreply.github.com>
2017-11-02 09:50:42 +0100

    Update ReservaSalas.java

--- app/src/main/java/com/example/rabanales21/rabanales21/ReservaSalas.java
+++ app/src/main/java/com/example/rabanales21/rabanales21/ReservaSalas.java
@@ -24,7 +24,7 @@ public class ReservaSalas extends Fragment implements View.OnClickListener {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_salacentauropeque, container, false);
+        return inflater.inflate(R.layout.fragment_reserva_salas, container, false);
     }
 
     @Override
