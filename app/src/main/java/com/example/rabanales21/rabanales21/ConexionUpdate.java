fde04c48ca616110bdf25cac1b4c1e27f0ba08c0
Villonder <32059608+Villonder@users.noreply.github.com>
2017-11-07 14:23:52 +0100

    cambio contraseña

---
+++ app/src/main/java/com/example/rabanales21/rabanales21/ConexionUpdate.java
@@ -0,0 +1,63 @@
+package com.example.rabanales21.rabanales21;
+
+import android.content.Intent;
+import android.os.AsyncTask;
+
+import org.json.JSONArray;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+class ConexionUpdate extends AsyncTask<String, Void, Integer> {
+
+    @Override
+    protected Integer doInBackground(String... params) {
+
+        StringBuilder resul;
+
+        Integer auxiliar = 0;
+
+        try {
+
+            URL url = new URL("https://www.rabanales21.com/modules_WebServices/" + params[0]);
+
+            HttpURLConnection conexion = (HttpURLConnection) url.openConnection();
+
+            int respuesta = conexion.getResponseCode();
+
+            resul = new StringBuilder();
+
+            if (respuesta == HttpURLConnection.HTTP_OK) {
+
+                InputStream in = new BufferedInputStream(conexion.getInputStream());
+
+                BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+
+                String linea;
+
+                while ((linea = reader.readLine()) != null) {
+
+                    resul.append(linea);
+
+                }
+
+            }
+
+            JSONArray json = new JSONArray(resul.toString());
+
+            auxiliar = (Integer) json.getJSONObject(0).get("resultado");
+
+        } catch (Exception e) {
+
+
+        }
+
+        return auxiliar;
+
+    }
+
+}
